5612bb983b6e3e6d78b91f009e3a0bbcf17c4e74
Chimll <107048935+Chimll@users.noreply.github.com>
2023-12-02 23:12:06 +0000

    changed assets
    changed assets and added a few while changing the spawn_items.cs code to allow for more assets for fruits. changed circle colliders to polygon colliders for more accurate slices.

--- Assets/Spawn_items.cs
+++ Assets/Spawn_items.cs
@@ -4,48 +4,52 @@ using UnityEngine;
 
 public class Spawn_items : MonoBehaviour
 {
-    public float spawnTime=1; //Spawn Time
-    public GameObject apple; //Apple prefab
-    public GameObject bomb; //Bomb prefab
-    public float upForce = 750; //Up force
-    public float leftRightForce = 200; //Left and right force
-    public float maxX = -7; //Max x spawn position
-    public float minX = 7; //Min x spawn position
+    public float spawnTime = 1; // Spawn Time
+    public GameObject[] fruits; // Array of fruit prefabs
+    public GameObject bomb; // Bomb prefab
+    public float upForce = 750; // Up force
+    public float leftRightForce = 200; // Left and right force
+    public float maxX = -7; // Max x spawn position
+    public float minX = 7; // Min x spawn position
 
     // Start is called before the first frame update
     void Start()
     {
-        //Start the spawn update
+        // Start the spawn update
         StartCoroutine("Spawn");
     }
 
     // Update is called once per frame
     IEnumerator Spawn()
     {
-        //Wait spawnTime
+        // Wait spawnTime
         yield return new WaitForSeconds(spawnTime);
-        //Spawn prefab is apple
-        int randomValue = Random.Range(0,100);
 
-        GameObject prefab = (randomValue < 30) ? bomb : apple;
+        // Randomly select a fruit prefab
+        GameObject prefab = fruits[Random.Range(0, fruits.Length)];
 
-        //Spawn prefab at random position
-        GameObject go = Instantiate(prefab,new Vector3(Random.Range(minX,maxX + 1),transform.position.y, 0f),Quaternion.Euler(0,0, Random.Range (-90F, 90F))) as GameObject;
+        // Randomly decide if spawning a bomb or a fruit
+        int randomValue = Random.Range(0, 100);
+        if (randomValue < 30) // 30% chance for a bomb
+        {
+            prefab = bomb;
+        }
 
-    //If x position is over 0 go left
+        // Spawn prefab at random position
+        GameObject go = Instantiate(prefab, new Vector3(Random.Range(minX, maxX + 1), transform.position.y, 0f), Quaternion.Euler(0, 0, Random.Range(-90F, 90F))) as GameObject;
+
+        // If x position is over 0 go left
         if (go.transform.position.x > 0)
         {
-            go.GetComponent<Rigidbody2D>().AddForce(new Vector2(-leftRightForce,upForce));
+            go.GetComponent<Rigidbody2D>().AddForce(new Vector2(-leftRightForce, upForce));
         }
-
-        //Else go right
+        // Else go right
         else
         {
-            go.GetComponent<Rigidbody2D>().AddForce(new Vector2(leftRightForce,upForce));
+            go.GetComponent<Rigidbody2D>().AddForce(new Vector2(leftRightForce, upForce));
         }
 
-        //Start the spawn again
+        // Start the spawn again
         StartCoroutine("Spawn");
     }
-    
 }
